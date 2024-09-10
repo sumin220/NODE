892c97d7836adcb3a4a37a80db4691bb6b32f6e4
sonsumin <sonsumin00@gmail.com>
2024-09-10 11:43:15 +0900

    Feat: 라우터 관련 처리

--- newQuiz/setting.js
+++ newQuiz/setting.js
@@ -1,5 +1,10 @@
 const express = require('express');
+var dbConfig = require('./config/db.js');
 var router = express.Router();
+
+// mysql 사용자 추가
+var conn = dbConfig.init();
+dbConfig.connect(conn);
 
 router.get('/', function(req, res) {
     res.send('setting root')
