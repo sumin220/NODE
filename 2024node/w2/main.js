5c6be8ad08ffba7813d437e7192d00dc82dc5001
sonsumin <sonsumin00@gmail.com>
2024-10-14 20:36:43 +0900

    Feat: fast response

--- 2024node/w2/main.js
+++ 2024node/w2/main.js
@@ -1,36 +1,21 @@
 var http = require('http');
 var fs = require('fs');
-const urlm = require("url");
+var urlm = require('url');
 var app = http.createServer(function (req, res) {
-    var _url = req.url;
-    var queryData = urlm.parse(_url, true).query;
-    console.log(_url);
+    var url = req.url;
+    var queryData = urlm.parse(url, true).query;
     console.log(queryData.id);
-
-    if (req.url == '/') {
-        _url = "/index.html";
+    var title = queryData.id;
+    console.log(url);
+    if (url = "/") {
+        title = 'welcome';
     }
-    if (req.url == '/favicon.ico') {
+    if (url = "/favicon.ico") {
         return res.writeHead(404);
     }
-    res.writeHead(200);
-    //readFileSync 매개변수로 넘겨진 변수를 읽음
-    var template = `
-   
-    <!doctype html>
-    <html>
-    <head>
-    <title>
-    WEB - ${queryData.id}
-    </title>
-</head>
-   h2 - ${queryData.id}
-    <body>
-    
-</body>
-</html>
-    `;
 
-    res.end(template);
+    res.writeHead(200);
+
+    var template = fs.readFileSync(__dirname + url);
 });
 app.listen(3000);
