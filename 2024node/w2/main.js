bd77ed22079a902eeb740c37a87d41c438f914de
sonsumin <sonsumin00@gmail.com>
2024-10-14 20:26:09 +0900

    Feat: fast response

--- 2024node/w2/main.js
+++ 2024node/w2/main.js
@@ -5,7 +5,7 @@ var app = http.createServer(function (req, res) {
     var _url = req.url;
     var queryData = urlm.parse(_url, true).query;
     console.log(_url);
-    console.log(queryData);
+    console.log(queryData.id);
 
     if (req.url == '/') {
         _url = "/index.html";
@@ -14,7 +14,23 @@ var app = http.createServer(function (req, res) {
         return res.writeHead(404);
     }
     res.writeHead(200);
-    //readFileSync 매개변수로 넘겨진 변수를 일긍ㅁ
-    res.end(fs.readFileSync(__dirname + url));
+    //readFileSync 매개변수로 넘겨진 변수를 읽음
+    var template = `
+   
+    <!doctype html>
+    <html>
+    <head>
+    <title>
+    WEB - ${queryData.id}
+    </title>
+</head>
+   h2 - ${queryData.id}
+    <body>
+    
+</body>
+</html>
+    `;
+
+    res.end(template);
 });
 app.listen(3000);
