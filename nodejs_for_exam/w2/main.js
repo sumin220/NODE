cb5880533b3196c8aaf5a7950e81b7cb3cb72acb
sonsumin <sonsumin00@gmail.com>
2024-10-14 20:12:05 +0900

    Feat: chap2

---
+++ nodejs_for_exam/w2/main.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var fs = require('fs');
+const urlm = require("url");
+var app = http.createServer(function (req, res) {
+    var _url = req.url;
+    var queryData = urlm.parse(_url, true).query;
+    console.log(_url);
+    console.log(queryData);
+
+    console.log(__dirname);
+    if (req.url == '/') {
+        _url = "/index.html";
+    }
+    if (req.url == '/main') {
+        _url = "/main.html";
+    }
+    if (req.url == '/favicon.ico') {
+        return res.writeHead(404);
+    }
+    res.writeHead(200);
+    //readFileSynd 매개변수로 넘겨진 변수를 일긍ㅁ
+    res.end(fs.readFileSync(__dirname + url));
+});
+app.listen(3000);
